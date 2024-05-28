f323dd1c83644ad40f470c6772262c459140496a
JorderGomes <jordernoveau@gmail.com>
2024-05-28 19:36:50 -0300

    feat: services - add observer, getter and setter for shared data

--- src/app/services/popup.service.ts
+++ src/app/services/popup.service.ts
@@ -1,16 +1,29 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PopupService {
 
-  showPopup:boolean = false;
+  // showPopup:boolean = false;
+
+  private showPopupSubject = new BehaviorSubject<boolean>(false);
+  showPopup$ = this.showPopupSubject.asObservable();
+
+  setShowPopup(show: boolean): void {
+    this.showPopupSubject.next(show);
+  }
+
+  getShowPopup(): boolean {
+    return this.showPopupSubject.value;
+  }
 
   constructor() { }
 
   toggleShowPopup(){
-    this.showPopup = !this.showPopup;
+    this.showPopupSubject.next(!this.showPopupSubject.value);
+    //this.showPopup = !this.showPopup;
   }
 
 }
--- src/app/services/transaction.service.ts
+++ src/app/services/transaction.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 // interface para transactions
 import { environment } from '../../environments/environment';
 import { Transaction } from '../Transaction';
@@ -12,6 +12,19 @@ export class TransactionService {
 
   private baseApiUrl:string = environment.baseApiUrl;
   private apiUrl:string = `${this.baseApiUrl}/registers`;
+  
+  private currentTransactionSubject = new BehaviorSubject<Transaction | null>(null);
+  currentTransaction$ = this.currentTransactionSubject.asObservable();
+
+  setCurrentTransaction(transaction: Transaction | null): void {
+    this.currentTransactionSubject.next(transaction);
+  }
+
+  getCurrentTransaction(): Transaction | null {
+    return this.currentTransactionSubject.value;
+  }
+
+
 
   constructor(private http: HttpClient) { }
 
@@ -25,6 +38,11 @@ export class TransactionService {
 
   createTransaction(transaction: Transaction): Observable<Transaction> {
     return this.http.post<Transaction>(this.apiUrl, transaction);
+  }
+
+  editTransaction(transaction: Transaction): Observable<Transaction> {
+    const url = `${this.apiUrl}/${transaction.id}`;
+    return this.http.put<Transaction>(url, transaction);
   }
 
 }
