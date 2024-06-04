f7e86a48f68384941acb88c9aedf4eef7011971d
JorderGomes <jordernoveau@gmail.com>
2024-06-04 17:37:20 -0300

    feat: add get metrics data methods

--- src/app/services/metric/metric.service.ts
+++ src/app/services/metric/metric.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { CostsByKey } from '../../interfaces/costs-by-key';
+import { Transaction } from '../../interfaces/Transaction';
+import { PercentualIncomeCosts } from '../../interfaces/percentual-income-costs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MetricService {
 
-  constructor() { }
+  private baseApiUrl:string = environment.baseApiUrl;
+  private apiUrl:string = `${this.baseApiUrl}/metrics`;
+
+  constructor(private http: HttpClient) { }
+
+  // todo: custos por tag
+  getCostsByKey(): Observable<CostsByKey[]>{
+    const url = `${this.apiUrl}/costs-by-tag`;
+    return this.http.get<CostsByKey[]>(url);
+  }
+
+  // todo: custos mais caros
+  getExpensiveCosts(): Observable<Transaction[]>{
+    const url = `${this.apiUrl}/expensive-costs`;
+    return this.http.get<Transaction[]>(url);
+  }
+
+  // todo: porcentagem de gastos e rendas
+  getPercentualIncomeCosts(): Observable<PercentualIncomeCosts[]>{
+    const url = `${this.apiUrl}/percentual-income-costs`;
+    return this.http.get<PercentualIncomeCosts[]>(url);
+  }
+
+  // todo: custos por mês
+  getCostsByMonth(): Observable<CostsByKey[]>{
+    const url = `${this.apiUrl}/costs-by-month`;
+    return this.http.get<CostsByKey[]>(url);
+  }
+  
+
 }
